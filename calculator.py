6242e3a562334e3a94097d9585bc9a92af3c45d0
unterorion <dydwlsdl9357@gmail.com>
2024-09-03 16:07:27 +0900

    new file:   calculator.py

---
+++ calculator.py
@@ -0,0 +1,41 @@
+class FourCal():
+    def __init__(self, input1, input2):
+        self.first = input1
+        self.second = input2
+
+    def sum(self):
+        return self.first + self.second
+    
+    def sub(self):
+        return self.first - self.second
+    
+    def prod(self):
+        return self.first * self.second
+    
+    def div(self):
+        return self.first / self.second
+
+a = FourCal(4.5, 2.8)
+print(a.sum())
+print(a.sub())
+print(a.prod())
+print(a.div())
+
+class PwrCal(FourCal):
+    def pwr(self):
+        return self.first**self.second
+    
+a = PwrCal(4.5, 2.8)
+print(a.pwr())
+
+class SafeCal(PwrCal):
+    def div(self):
+        if self.second==0:
+            return 0
+        else:
+            return self.first / self.second
+        
+a = SafeCal(4.5, 2.8)
+b = SafeCal(4.5, 0)
+print(a.div())
+print(b.div())
