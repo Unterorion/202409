a8f7beb42666fd14ed7f8a19b128a0233d91e7c3
unterorion <dydwlsdl9357@gmail.com>
2024-09-03 14:08:54 +0900

    modified:   function.py

--- function.py
+++ function.py
@@ -25,3 +25,29 @@ def print_dict(**kwarg):
 print_dict(a=1, b='python')
 
 print("")
+
+#초기값
+def intro(name, age, male=True):
+    print("My name is %s."%name)
+    print("I'm %d years old."%age)
+    if male:
+        print("I'm male.")
+    else:
+        print("I'm female.")
+
+intro('kyj', 30)
+intro('kyj', 30, True)
+intro('jea', 27, False)
+
+print("")
+
+#리턴 개수
+def add_mul(a,b):
+    return a+b, a*b
+
+result = add_mul(3,4)
+print(result)
+
+result1, result2 = add_mul(3,4)
+print(result1)
+print(result2)
